c45458a39e7a7d03cf4af8855bcad33a52c33d21
xanderflood <xander.t.flood@gmail.com>
2017-10-26 23:19:08 -0400

    Trying some ways to fix the struct->int thing

--- main.c
+++ main.c
@@ -5,14 +5,15 @@
 enum CXChildVisitResult partVisitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
   // "client_data" is a string prefix, like "arg:", "member:", "value:" for each case
   if (cursor.kind == CXCursor_ParmDecl || cursor.kind == CXCursor_TypeRef)
-    printf("%s:%s:%s\n", (char*)client_data,
-      clang_getCString(clang_getTypeSpelling(clang_getCursorType(cursor))),
-      clang_getCString(clang_getCursorSpelling(cursor)));
+    printf("%s:%s\n", (char*)client_data,
+      clang_getCString(clang_getTypeSpelling(clang_getCursorType(cursor))));
 
   return CXChildVisit_Continue;
 }
 
 enum CXChildVisitResult statementVisitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
+  int i;
+
   CXSourceLocation cxsl = clang_getCursorLocation(cursor);
   if (!clang_Location_isFromMainFile(cxsl))
     return CXChildVisit_Continue;
@@ -20,18 +21,24 @@ enum CXChildVisitResult statementVisitor(CXCursor cursor, CXCursor parent, CXCli
   if (cursor.kind == CXCursor_FunctionDecl) {
     printf("function:%s\n", clang_getCString(clang_getCursorSpelling(cursor)));
     printf("return:%s\n", clang_getCString(clang_getTypeSpelling(clang_getCursorResultType(cursor))));
+
+    // CXType fdecl = clang_getCursorType(cursor);
+    // for (i = 0; i < clang_getNumArgTypes(fdecl); i++) {
+    //   printf("arg:%s:%s\n", clang_getCString(clang_getTypeSpelling(clang_getArgType(fdecl, i))));
+    // }
+
     clang_visitChildren(cursor, partVisitor, "arg");
   }
 
-  if (cursor.kind == CXCursor_StructDecl) {
-    printf("struct\n");
-    clang_visitChildren(cursor, partVisitor, "member");
-  }
+  // if (cursor.kind == CXCursor_StructDecl) {
+  //   printf("struct\n");
+  //   clang_visitChildren(cursor, partVisitor, "member");
+  // }
 
-  if (cursor.kind == CXCursor_EnumDecl) {
-    printf("enum\n");
-    clang_visitChildren(cursor, partVisitor, "arg");
-  }
+  // if (cursor.kind == CXCursor_EnumDecl) {
+  //   printf("enum\n");
+  //   clang_visitChildren(cursor, partVisitor, "arg");
+  // }
 
   printf("\n");
 
